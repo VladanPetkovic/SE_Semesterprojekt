24a0bfa9bae4f55106ff201469ed8775138d54d0
Vladan P <vladan.petkovic27@gmail.com>
2023-10-09 10:06:13 +0200

    minor changes

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,10 +1,12 @@
 package org.example;
 
+import java.util.ArrayList;
+
 public class Main {
     public static void main(String[] args)
     {
-        Game game = new Game();
-        game.startGame();
-        game.endGame();
+        //Game game = new Game();
+        //game.startGame();
+        //game.endGame();
     }
 }
